4c71667c7918c1e8d16479aeba6aef736a8c20c5
Shuyue Zhang <shuyuezhang@macbook-pro.mynetworksettings.com>
2025-04-19 18:40:32 -0400

    fix DB collection

--- kambaz-node-server-app/Kambaz/Quizzes/Questions/model.js
+++ kambaz-node-server-app/Kambaz/Quizzes/Questions/model.js
@@ -1,5 +1,6 @@
 // Kambaz/Modules/Questions/model.js
 import mongoose from "mongoose";
 import schema from "./schema.js";
-const model = mongoose.model("QuestionModel", schema);
-export default model;
+// const model = mongoose.model("QuestionModel", schema);
+const questionModel = mongoose.model("Question", schema, "questions");
+export default questionModel;
