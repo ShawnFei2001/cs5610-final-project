477dfdd9325356d1e456e925d72bc2fd61cbcebc
Shawn Fei <shawnfei2001@gmail.com>
2025-04-20 23:36:06 -0400

    fix deploy problem

--- kambaz-react-web-app/src/Kambaz/Courses/Quizzes/client.ts
+++ kambaz-react-web-app/src/Kambaz/Courses/Quizzes/client.ts
@@ -1,7 +1,7 @@
 // src/Kambaz/Courses/Quizzes/client.ts
 import axios from "axios";
 
-const REMOTE_SERVER = import.meta.env.VITE_REMOTE_SERVER || "http://localhost:4000";
+const REMOTE_SERVER = import.meta.env.VITE_REMOTE_SERVER_A6 || "http://localhost:4000";
 const QUIZZES_API = `${REMOTE_SERVER}/api`;
 
 export const createQuizForCourse = async (courseId: string, quiz: any) => {
